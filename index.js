b5d4ed0131c1ff32d9485ded813967d1c3b7d418
Justin Tormey <justin@blockchain.com>
2017-04-29 11:50:15 -0400

    support worker pools

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ class Battalion {
     return new WorkerInstance(this.WorkerClass, options)
   }
 
+  createPool (count, localOptions) {
+    if (!count) throw new Error('Count is required')
+    return new PoolInstance(this.WorkerClass, Object.assign({ count }, localOptions, this._options))
+  }
+
   static export (context, methods) {
     let sendResponse = (_id, value) => {
       context.postMessage({ _type: RETURN_TYPE, _id, value })
@@ -51,14 +56,19 @@ class WorkerInstance {
     this._options = options
   }
 
+  get pending () {
+    return Object.keys(this._pending).length
+  }
+
   handleMessage (event) {
     let { _type, _id, value } = event.data
-    if (_type === RETURN_TYPE) {
-      this._pending[_id].resolve(value)
-    } else if (_type === ERROR_TYPE) {
-      this._pending[_id].reject(new Error(value))
-    }
+    let resolver = this._pending[_id]
     delete this._pending[_id]
+    if (_type === RETURN_TYPE) {
+      resolver.resolve(value)
+    } else if (_type === ERROR_TYPE) {
+      resolver.reject(new Error(value))
+    }
   }
 
   dispatch (_type, data) {
@@ -68,6 +78,63 @@ class WorkerInstance {
     let resolver = (resolve, reject) => {
       this._pending[_id] = { resolve, reject }
       this._worker.postMessage(Object.assign({}, data, { _type, _id }))
+    }
+
+    if (Task) {
+      return new Task((reject, resolve) => resolver(resolve, reject))
+    } else {
+      return new P(resolver)
+    }
+  }
+}
+
+class PoolInstance {
+  constructor (WorkerClass, options = {}) {
+    this._queue = []
+    this._pending = {}
+    this._options = options
+
+    this._workers = [...new Array(options.count)].map(_ =>
+      new WorkerInstance(WorkerClass, options)
+    )
+  }
+
+  get pending () {
+    return this._workers.map(w => w.pending).reduce((a, b) => a + b, 0)
+  }
+
+  schedule () {
+    let { Task } = this._options
+    this._workers.filter(w => {
+      return w.pending === 0
+    }).forEach(w => {
+      if (this._queue.length) {
+        let next = this._queue.shift()
+        let { resolve, reject } = this._pending[next._id]
+        if (Task) {
+          w.dispatch(next._type, next.data).fork(reject, (value) => {
+            delete this._pending[next._id]
+            resolve(value)
+          })
+        } else {
+          w.dispatch(next._type, next.data).then(resolve, reject).then(() => {
+            delete this._pending[next._id]
+          })
+        }
+      }
+    })
+  }
+
+  dispatch (_type, data) {
+    let _id = uniqueId()
+    let { Promise: P = Promise, Task } = this._options
+
+    let resolver = (res, rej) => {
+      this._queue.push({ _id, _type, data })
+      let resolve = (x) => { res(x); this.schedule() }
+      let reject = (x) => { rej(x); this.schedule() }
+      this._pending[_id] = { resolve, reject }
+      this.schedule()
     }
 
     if (Task) {
